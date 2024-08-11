ef454376fcd22c3fbe8ad7838807d309b4a72a41
melikaouididi <melikaouididi@hotmail.com>
2024-08-11 13:59:57 +0100

    Backend Development with Golang

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/gin-gonic/gin"
+	"github.com/melikaouididi/Ecommerce-Project/controllers"
+	"github.com/melikaouididi/Ecommerce-Project/database"
+	"github.com/melikaouididi/Ecommerce-Project/middleware"
+	"github.com/melikaouididi/Ecommerce-Project/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run(":" + port))
+}
